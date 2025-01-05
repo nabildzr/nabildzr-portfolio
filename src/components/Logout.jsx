4d6e785b2155e93513586ccb97a73a7d1e71380b
nabildzr <muhammadnbillz@gmail.com>
2025-01-05 11:55:43 +0700

    Push

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,23 +1,15 @@
 // filepath: /C:/Users/M Nabil Dzikrika R/Documents/Portfolio/Portfolio-1/portfolio-deploy-2/src/components/Logout.jsx
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    Swal.fire({
-      title: "Logged Out",
-      text: "You have been logged out successfully.",
-      icon: "success",
-      timer: 3000,
-      showConfirmButton: false,
-    }).then(() => {
-      window.location.href = '/?logout=true';
-    });
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    window.location.href = "/?logout=true";
   }, [navigate]);
 
   return null;
